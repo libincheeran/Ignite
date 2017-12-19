556e806afc211037a930726628ede79ac8c963aa
Libin Cheeran <lcheeran@pdxinc.com>
2017-12-19 16:33:39 -0600

    First Commit

---
+++ src/main/java/com/libin/TestServlet.java
@@ -0,0 +1,20 @@
+package com.libin;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet( "/TestServlet")
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Hello");
+        response.getOutputStream().print("Hello form the other side");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doPost(request, response);
+    }
+}
